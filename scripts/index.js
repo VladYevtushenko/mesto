c09e35aa25b1b2aebbbeb97ea4041ef9652ea213
Vladislav Yevtushenko <vladislavyevtushenko@yandex.ru>
2022-01-30 20:11:56 +0300

    feat: add JS cards unboxing/like-button/dlete-button

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,83 @@
-import  { initialCards } from "./elements"
+const initialCards = [
+    {
+        name: 'Карачаево-Черкесия',
+        link: '../images/kirill-pershin-1088404-unsplash.png',
+    },
+    {
+        name: 'Гора Эльбрус',
+        link: '../images/kirill-pershin-1404681-unsplash.png',
+    },
+    {
+        name: 'Домбай',
+        link: '../images/kirill-pershin-1556355-unsplash.png',
+    },
+    {
+        name: 'Москва-Сити',
+        link: '../images/alexandr-bormotin-jj3PpeBIlLA-unsplash.jpg',
+    },
+    {
+        name: 'Морпорт Сочи',
+        link: '../images/dima-fedorov-ZZkw00YCY1E-unsplash.jpg',
+    },
+    {
+        name: 'Санкт-Петербург',
+        link: '../images/ilya-bronskiy-7haj8Ca19Ts-unsplash.jpg',
+    },
+];
 
 const profileEditing = document.querySelector('.profile__edit-button');
-
 const popupClose = document.querySelector('.popup__close-button');
-
 const popup = document.querySelector('.popup');
-
 const profileName = document.querySelector('.profile__name');
-
 const profileAboutMe = document.querySelector('.profile__about-me');
-
 const userName = document.querySelector('#popupProfName');
-
 const userAboutMe = document.querySelector('#popupProfAboutMe');
-
 const popupForm = document.querySelector('.popup__container');
+
+let stateCards = [...initialCards];
+const template = document.querySelector('#element-template');
+const container = template.parentElement;
+const cardTemplate = template.content;
+
+const clearCards = () => {
+    container.querySelectorAll('.elements__card').forEach((cardElement, index) => cardElement.remove());
+};
+
+const addCard = (card, index) => {
+    const cardElement = cardTemplate.querySelector('.elements__card').cloneNode(true);
+    cardElement.querySelector('.elements__photo').alt = card.name;
+    cardElement.querySelector('.elements__title').textContent = card.name;
+    cardElement.querySelector('.elements__photo').src = card.link;
+
+    const likeButton = cardElement.querySelector('.elements__like-button');
+    likeButton.onclick = () => {
+        likeButton.classList.toggle('elements__like-button_active');
+    };
+
+    const deleteButton = cardElement.querySelector('.elements__delete-button');
+    deleteButton.onclick = () => {
+    stateCards = [...stateCards.slice(0, index),...stateCards.slice(index + 1),];
+    clearCards();
+    renderCards();
+    };
+    container.append(cardElement);
+};
+
+const renderCards = () => {
+    stateCards.forEach(addCard);
+};
+
+renderCards();
+
+// addCard({
+//     name: 'Гора Эльбрус2',
+//     link: '../images/kirill-pershin-1404681-unsplash.png',
+//   }, initialCards.length);
 
 function profilePopupOpen() {
     userName.value = profileName.textContent;
     userAboutMe.value = profileAboutMe.textContent;
-    popup.classList.add('popup_opened'); 
+    popup.classList.add('popup_opened');
 }
 
 function profilePopupClose() {
