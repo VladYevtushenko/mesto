c73b1b3cc5e2418eade99e2d9090a5ac6a43dddd
Vladislav Yevtushenko <vladislavyevtushenko@yandex.ru>
2022-03-23 18:54:47 +0300

    index.js refactoring

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import { initialCards } from "../utils/consts.js";
 import { Card } from "../components/Card.js";
 import { config, FormValidator } from "../components/FormValidator.js";
-import { popups, popupProfile, profileEditButton, profileForm, profileName, profileAboutMe, userName, userAboutMe, elements, container, popupViewImage, popupImage, popupImageTitle, popupCard, popupCardAddButton, popupCardForm, popupCardName, popupCardLink } from "../utils/consts.js";
+import { initialCards, popupProfile, profileEditButton, userName, userAboutMe, popupCard, popupCardAddButton, popupCardForm, popupCardName, popupCardLink } from "../utils/consts.js";
 import { Section } from "../components/Section.js";
 import { PopupWithImage } from "../components/PopupWithImage.js";
 import { PopupWithForm } from "../components/PopupWithForm.js";
@@ -14,24 +13,6 @@ const userInfo = new UserInfo({
     userNameSelector: '.profile__name',
     aboutMeSelector: '.profile__about-me',
 });
-
-
-// function openPopup(popups) {
-//     popups.classList.add('popup_opened');
-//     document.addEventListener('keydown', closeByEscapeBtn);
-// }
-
-// function closePopup(popups) {
-//     popups.classList.remove('popup_opened');
-//     document.removeEventListener('keydown', closeByEscapeBtn);
-// }
-
-// function closeByEscapeBtn (evt) {
-//     if (evt.key === 'Escape') {
-//         const openedPopup = document.querySelector('.popup_opened');
-//         closePopup(openedPopup);
-//     }
-// }
 
 validateProfileForm.enableValidation();
 validateNewCardForm.enableValidation();
@@ -58,34 +39,6 @@ const openProfilePopup = () => {
 
 profileEditButton.addEventListener('click', openProfilePopup);
 
-
-// function handleProfileFormSubmit (evt) {
-//     evt.preventDefault();
-//     profileName.textContent = userName.value;
-//     profileAboutMe.textContent = userAboutMe.value;
-//     closePopup(popupProfile);
-// }
-
-// profileForm.addEventListener('submit', handleProfileFormSubmit);
-
-// function editProfilePopup() {
-//     userName.value = profileName.textContent;
-//     userAboutMe.value = profileAboutMe.textContent;
-//     openPopup(popupProfile);
-//     validateProfileForm.resetValidation();
-// }
-
-// profileEditButton.addEventListener('click', editProfilePopup);
-
-// initialCards.forEach((element) => {
-//     const cardElement = createCard(element);
-//     addCard(cardElement, elements);
-// });
-
-// function addCard(card) {
-//     container.prepend(card)
-// }
-
 const imagePopup = new PopupWithImage('.popup_type_image');
 
 imagePopup.setEventListeners();
@@ -110,34 +63,6 @@ const cardsList = new Section({
 
 cardsList.renderItems(initialCards);
 
-// function viewImage(name, link) {
-//     popupImageTitle.textContent = name;
-//     popupImage.src = link;
-//     popupImage.alt = link;
-
-//     openPopup(popupViewImage);
-// }
-// function openCardPopup() {
-//     popupCardForm.reset();
-//     openPopup(popupCard);
-//     validateNewCardForm.resetValidation();
-// }
-// popupCardAddButton.addEventListener('click', openCardPopup);
-
-// function handleNewCardPopupForm (evt) {
-//     evt.preventDefault();
-
-//     const newCard = {
-//         name: popupCardName.value,
-//         link: popupCardLink.value,
-//     };
-//     const cardElement = createCard(newCard);
-
-//     addCard(cardElement, elements);
-//     closePopup(popupCard);
-//     popupCardForm.reset();
-// }
-
 const newCardPopup = new PopupWithForm({ 
     popupSelector: '.popup_type_card',
     handleFormSubmit: () => {
@@ -159,19 +84,3 @@ const openNewCardPopup = () => {
 }
 
 popupCardAddButton.addEventListener('click', openNewCardPopup);
-
-// popupCardForm.addEventListener('submit', handleNewCardPopupForm);
-
-// popups.forEach((popup) => {
-//     popup.addEventListener('mousedown', (evt) => {
-//         if (evt.target.classList.contains('popup_opened')) {
-//             closePopup(popup);
-//         }
-//         if (evt.target.classList.contains('popup__close-button')) {
-//             closePopup(popup);
-//         }
-//     });
-// });
-
-
-
